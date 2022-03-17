abfc44d39e281fc29956b28f54274d17a39da0aa
blox <72946059+coolpro200021@users.noreply.github.com>
2022-03-17 11:07:46 -0500

    Added Command function
    Command function allows a command line within VSCode

--- server.py
+++ server.py
@@ -4,25 +4,17 @@ from flask import Flask, request
 import random
 from threading import Thread
 from replit import db
-
+db = []
 app = Flask(__name__)
 def find(code):
   found = False
   for i in db:
     print(i)
     if str(i[1]) == str(code):
-      print(i)
       found = True
       return db.index(i)
   if found == False:
     return "None"
-
-def gen():
-  new = random.randint(10000,9999999)
-  if find(new):
-    return gen()
-  else:
-    return new
 
 @app.route('/')
 def page1():
@@ -30,8 +22,8 @@ def page1():
 
 @app.route('/new')
 def page2():
-  new = gen()
-  db.append(["", new])
+  new = random.randint(10000,9999999)
+  db.append(["", new, ""])
   return str(new)
 
 @app.route('/data/<code>', methods = ["GET", "POST"])
@@ -41,11 +33,34 @@ def page3(code):
   else:
     index = find(code)
     if request.method == "GET":
-      return db[find(code)][0]
+      return db[index][0]
     else:
-      db[find(code)] = [request.form, code]
-      return "Success"
-  
+      oldData = db[index][2]
+      db[index] = [request.form, code, oldData]
+      return "Done"
+
+@app.route('/delete/<code>')
+def page4(code):
+  if find(code) == "None":
+    return "Not found"
+  else:
+    del db[find(code)]
+    return "Successfully deleted "+code
+
+@app.route('/command/<code>', methods = ["GET", "POST"])
+def page5(code):
+  if find(code) == "None":
+    return "Not found"
+  else:
+    index = find(code)
+    if request.method == "GET":
+      data = db[index][2]
+      db[index][2] = ""
+      return data
+    else:
+      oldData = db[index][1]
+      db[index] = [oldData, code, request.form]
+      return "Done"
 # set website
 def run():
     app.run(host="0.0.0.0", port=8080)
