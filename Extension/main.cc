7169027af9719e2234946ad5440009a4c0aca5f8
blox <72946059+coolpro200021@users.noreply.github.com>
2022-03-17 14:06:21 -0500

    Bug fixes, Added main functionality

--- Extension/main.cc
+++ Extension/main.cc
@@ -1,26 +1,46 @@
 #include <string>
 #include <iostream>
-#include <nbind>
+#include "nbind/nbind.h"
+#include <fstream>
+using namespace std;
 
+static string createFile(std::string where, std::string name, std::string data){
+  ofstream NewFile(name.append(".lua"));
+  NewFile << data;
+  NewFile.close();
+  return "";
+}
+static int writeFile(std::string file, std::string newData){
+  ofstream writer(file);
+  if (! writer){
+    cout << "Error writing .lua files" << endl;
+    return -1;
+  }
+  writer << newData << endl;
+  writer.close();
+}
 struct Main {
   static void newCode() {
-    
-  }, 
+    return;
+  };
 
   static void getData(std::string code) {
     
-  },
+  };
 
   static void installPlugin() {
     
-  },
+  };
 
   static void build() {
     
-  },
+  };
 };
 
  
 NBIND_CLASS(Main) {
-  method(sayHello);
+  method(newCode);
+  method(build);
+  method(installPlugin);
+  method(getData);
 }
